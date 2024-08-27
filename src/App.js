fde8a5373688527bfaf237cc4cb93cf90dab03dc
Claudinei <claudineimoreirax7l@gmail.com>
2024-08-27 09:47:38 -0300

    Criação do menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import Toolbar from './Toolbar/component/Toolbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <PrimeReactProvider>
+      <div style={{marginTop:'0.5%'}}>
+        <Toolbar /> 
+      </div>
+    </PrimeReactProvider>
+);
 }
 
 export default App;
